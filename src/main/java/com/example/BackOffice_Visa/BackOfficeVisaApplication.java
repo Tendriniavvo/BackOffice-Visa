1661c24adfe8fa7587924c490473467af550cefe
Tendriniavvo <tendryniavo76@gmail.com>
2026-04-16 08:27:28 +0300

    Initial commit

---
+++ src/main/java/com/example/BackOffice_Visa/BackOfficeVisaApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackOffice_Visa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackOfficeVisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackOfficeVisaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BackOffice_Visa/BackOfficeVisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackOffice_Visa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackOfficeVisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
